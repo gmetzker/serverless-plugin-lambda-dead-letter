7ab21815eac22c45d8ea901ce724356e4d365033
Gary Metzker <gmetzker@gmail.com>
2017-01-05 08:55:30 -0800

    Code to updateFunctionConfiguration:

--- src/index.js
+++ src/index.js
@@ -10,24 +10,26 @@ class Plugin {
     this.hooks = {
        // 'deploy:compileEvents': this.compileLambdaDeadLetterResources.bind(this),
       'deploy:deploy': this.setLambdaDeadLetterConfig.bind(this),
-      'setLambdaDeadLetterConfig:setLambdaDeadLetterConfig': this.setLambdaDeadLetterConfig.bind(this),
+      'setLambdaDeadLetterConfig:setLambdaDeadLetterConfig': this.setLambdaDeadLetterConfig.bind(this)
     };
 
     this.commands = {
       setLambdaDeadLetterConfig: {
         usage: 'Adds subscriptions to any SNS Topics defined by externalSNS.',
-        lifecycleEvents: ['setLambdaDeadLetterConfig'],
-      },
+        lifecycleEvents: ['setLambdaDeadLetterConfig']
+      }
     };
 
   }
 
   setLambdaDeadLetterConfig() {
 
+    // TODO:  Rework t his to wait until all promics are complete.
     this.serverless.service.getAllFunctions().forEach((functionName) => {
 
       const functionObj = this.serverless.service.getFunction(functionName);
-      let targetArnString = '';
+      let targetArnString;
+
 
       this.serverless.cli.log(`** Function:  ${functionName}`);
 
@@ -37,24 +39,45 @@ class Plugin {
       if (!functionObj.deadLetter.targetArn) {
         throw new Error(`Function: ${functionName} is missing 'targetArn' value.`);
       }
+      const targetArn = functionObj.deadLetter.targetArn;
 
-      if (typeof functionObj.deadLetter.targetArn === 'string') {
+      if (typeof targetArn === 'string') {
         const rg = new RegExp(
           '^(arn:aws:sqs:[a-z]{2,}-[a-z]{2,}-[0-9]{1}:[0-9]{12}:[a-zA-z0-9\\-_.]{1,80}$' +
           '|arn:aws:sns:[a-z]{2,}-[a-z]{2,}-[0-9]{1}:[0-9]{12}:[a-zA-z0-9\\-_]{1,256})$');
 
 
-        if (!rg.test(functionObj.deadLetter.targetArn)) {
+        if (!rg.test(targetArn)) {
           throw new Error(`Function property ${functionName}.deadLetter.targetArn = '${functionObj.deadLetter.targetArn}'.  This is not a valid sns or sqs arn. `);
         }
 
-        targetArnString = functionObj.deadLetter.targetArn;
+        targetArnString = targetArn;
 
-      } else if (typeof functionObj.deadLetter.targetArn === 'object') {
-        return;
+      } else if (typeof targetArn === 'object') {
+
+        if (!targetArn.GetArn) {
+          throw new Error(`Function property ${functionName}.deadLetter.targetArn object is missing GetArn property.`);
+        }
+        // let dlLogicalId = targetArn.GetArn;
+
+        // TODO:  Check that dlLogicalId is a valid resource in the stack
+        // TODO:  Attempt to get the ARN of the resource identified by dlLogicalId
+        // targetArnString ...
       }
 
       this.serverless.cli.log(`** Function: ${functionName}, DeadLetterArn:  ${targetArnString}`);
+
+      const params = {
+        FunctionName: functionObj.name,
+        DeadLetterConfig: {
+          TargetArn: targetArnString
+        }
+      };
+
+      return this.provider.request('Lambda', 'updateFunctionConfiguration', params, this.options.stage, this.options.region)
+             .then(() => {
+               this.serverless.cli.log(`Function '${functionName}' DeadLetterConfig assigned.`);
+             });
 
     });
   }
